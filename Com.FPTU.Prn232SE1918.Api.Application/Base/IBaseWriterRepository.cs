062f612aa7d6d3baa5796f9b48e9356bf7cece72
Bui Huu Dong <buihuudong19@gmail.com>
2026-05-21 14:36:14 +0700

    Commit interfaces

--- Com.FPTU.Prn232SE1918.Api.Application/Base/IBaseWriterRepository.cs
+++ Com.FPTU.Prn232SE1918.Api.Application/Base/IBaseWriterRepository.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Com.FPTU.Prn232SE1918.Api.Application.Base;
-public interface IBaseWriterRepository
+public interface IBaseWriterRepository<T> : IBaseRepo<T> where T : class
 {
-
+    Task InsertAsync(T entity, bool saveChanges = true);
+    Task InsertRangeAsync(IEnumerable<T> entities, bool saveChanges = true);
+    Task UpdateAsync(T entity, bool saveChanges = true);
+    Task UpdateRangeAsync(IEnumerable<T> entities, bool saveChanges = true);
+    Task DeleteAsync(T entity, bool saveChanges = true);
+    /*Overloading method*/
+    Task DeleteAsync(int id, bool saveChanges = true);
+    Task DeleteRangeAsync(IEnumerable<T> entities, bool saveChanges = true);
 }
