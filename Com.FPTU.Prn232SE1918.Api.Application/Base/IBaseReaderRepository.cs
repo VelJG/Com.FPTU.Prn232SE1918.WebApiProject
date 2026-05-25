151dfd608246525521881ad7744b93cba526505c
Bui Huu Dong <buihuudong19@gmail.com>
2026-05-25 12:54:48 +0700

    Commit reader

--- Com.FPTU.Prn232SE1918.Api.Application/Base/IBaseReaderRepository.cs
+++ Com.FPTU.Prn232SE1918.Api.Application/Base/IBaseReaderRepository.cs
@@ -4,6 +4,21 @@ using System.Text;
 
 namespace Com.FPTU.Prn232SE1918.Api.Application.Base;
 
-public interface IBaseReaderRepository
+public interface IBaseReaderRepository<T>: IBaseRepo<T> where T : class
 {
+    /// <summary>
+    /// Get all items of an entity by asynchronous (bat dong bo)
+    /// </summary>
+    /// <returns>List of T</returns>
+    Task<IList<T>> GetAllAsync();
+
+    T Find(params object[] keyValues);
+    // T Find(object key);
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="keyValues"></param>
+    /// <returns></returns>
+    Task<T> FindAsync(params object[] keyValues);
+
 }
