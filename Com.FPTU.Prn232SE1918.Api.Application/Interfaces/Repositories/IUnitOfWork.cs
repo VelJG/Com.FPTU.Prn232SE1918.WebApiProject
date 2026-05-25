a31fb86723e9cb2da6c07e54539370fa49be0443
Bui Huu Dong <buihuudong19@gmail.com>
2026-05-25 13:35:26 +0700

    Commit IoW

---
+++ Com.FPTU.Prn232SE1918.Api.Application/Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿namespace Com.FPTU.Prn232SE1918.Api.Application.Interfaces.Repositories;
+
+public interface IUnitOfWork
+{
+    /// <summary>
+    /// Application db context
+    /// </summary>
+    IApplicationDbContext ApplicationDbContext { get; }
+    /// <summary>
+    /// Get repository instance of an entity (T) inside Repository
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    IRepository<T> Repository<T>() where T : class; //has-a: day la mot cai ham
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    Task BeginTransactionAsync();
+    Task CommitTransactionAsync();
+    Task RolbackTransactionAsync();
+
+}
