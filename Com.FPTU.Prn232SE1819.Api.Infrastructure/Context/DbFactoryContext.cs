37fbf42bd0676b3e48460a95aad8dca193cdb239
Bui Huu Dong <buihuudong19@gmail.com>
2026-05-28 13:46:32 +0700

    func delegate

---
+++ Com.FPTU.Prn232SE1819.Api.Infrastructure/Context/DbFactoryContext.cs
@@ -0,0 +1,31 @@
+﻿using Com.FPTU.Prn232SE1918.MssqlServer.Entity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Com.FPTU.Prn232SE1819.Api.Infrastructure.Context;
+/// <summary>
+/// Represents a factory context for managing database connections or operations.
+/// </summary>
+public class DbFactoryContext
+{
+    //fields
+    private DbContext _dbContext;
+    private Func<ProductDbContext> _instanceFunc;
+
+    //properties
+    public DbContext DbContext => this._dbContext ?? (this._dbContext = _instanceFunc.Invoke());
+    
+    //Constructor
+    public DbFactoryContext(Func<ProductDbContext> dbContextFactory)
+    {
+        _instanceFunc = dbContextFactory;
+    }
+    //public DbContext GetDbContext()
+    //{
+    //    if (_dbContext == null)
+    //    {
+    //        _dbContext = _instanceFunc(); // Gọi factory method để tạo
+    //    }
+    //    return _dbContext;
+    //}
+
+}
