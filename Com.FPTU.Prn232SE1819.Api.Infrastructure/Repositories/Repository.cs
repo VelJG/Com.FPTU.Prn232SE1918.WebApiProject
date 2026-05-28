703a46e2b990b416689349d4f1ea7226798f4b77
Bui Huu Dong <buihuudong19@gmail.com>
2026-05-28 14:09:52 +0700

    commit implement interfaces

---
+++ Com.FPTU.Prn232SE1819.Api.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,61 @@
+﻿using Com.FPTU.Prn232SE1918.Api.Application.Interfaces.Common;
+using Com.FPTU.Prn232SE1918.Api.Application.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+namespace Com.FPTU.Prn232SE1819.Api.Infrastructure.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    public DbSet<T> Entities => throw new NotImplementedException();
+
+    public IApplicationDbContext ApplicationDbContext => throw new NotImplementedException();
+
+    public Task DeleteAsync(T entity, bool saveChanges = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id, bool saveChanges = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteRangeAsync(IEnumerable<T> entities, bool saveChanges = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Find(params object[] keyValues)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> FindAsync(params object[] keyValues)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IList<T>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task InsertAsync(T entity, bool saveChanges = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task InsertRangeAsync(IEnumerable<T> entities, bool saveChanges = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(T entity, bool saveChanges = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateRangeAsync(IEnumerable<T> entities, bool saveChanges = true)
+    {
+        throw new NotImplementedException();
+    }
+}
