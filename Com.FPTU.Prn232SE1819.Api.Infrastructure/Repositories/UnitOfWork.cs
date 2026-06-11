1c129b18ab27d55821de6a628cd1c4a333a31833
Bui Huu Dong <buihuudong19@gmail.com>
2026-06-11 13:43:16 +0700

    commit repo instance

--- Com.FPTU.Prn232SE1819.Api.Infrastructure/Repositories/UnitOfWork.cs
+++ Com.FPTU.Prn232SE1819.Api.Infrastructure/Repositories/UnitOfWork.cs
@@ -27,25 +27,48 @@ public class UnitOfWork : IUnitOfWork
 
     }
 
-
     public IRepository<T> Repository<T>() where T : class
     {
-        throw new NotImplementedException();
+        //lam sao de return 1 repository cua mot entity T nao do?
+        var type = typeof(T);//Muon lay kieu cua entity T
+        var typeName = type.Name;
+        //lock: giup tuan tu hoa neu nhu co nhieu loi de tao repo
+        lock (Repositories)
+        {
+            if (Repositories.ContainsKey(typeName))
+                return (IRepository<T>)Repositories[typeName];
+            //tao moi repo tuong ung cho kieu T di vao
+            var repository = new Repository<T>(ApplicationDbContext);
+            //add vo cached 
+            Repositories.Add(typeName, repository);
+
+            return repository;
+        }
     }
 
     public async Task BeginTransactionAsync()
     {
         if (_transaction == null)
         {
+            // Thông báo cho EF Core rằng chúng ta muốn bắt đầu một transaction mới
+            // với mức độ cô lập đã chỉ định (nếu có) --> Chưa được commit hoặc rollback
             _transaction = _isolationLevel.HasValue ?
                 await ApplicationDbContext.DbContext.Database.BeginTransactionAsync(_isolationLevel.GetValueOrDefault()) :
                 await ApplicationDbContext.DbContext.Database.BeginTransactionAsync();
         }
     }
 
-    public Task CommitTransactionAsync()
+    public async Task CommitTransactionAsync()
     {
-        throw new NotImplementedException();
+        //apply (commit) toan bo transactions => database
+        await ApplicationDbContext.DbContext.SaveChangesAsync(); 
+
+        if (_transaction is null) return;
+        //clean toan bo hanh dong da duoc apply vao db (xóa toàn bộ trên vùng nhớ tạm của EFC)
+        await _transaction.CommitAsync();
+        //nothing vung nho dem (neu co)
+        await _transaction.DisposeAsync(); 
+        _transaction = null;
     }
 
     public Task RollbackTransactionAsync()
