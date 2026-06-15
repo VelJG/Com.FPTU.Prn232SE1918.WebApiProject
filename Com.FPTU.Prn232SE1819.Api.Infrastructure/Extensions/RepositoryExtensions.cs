c92d562c377f0275a360dd2d40a062f0c3a8f3b6
Bui Huu Dong <buihuudong19@gmail.com>
2026-06-15 14:08:25 +0700

    commit extension

--- Com.FPTU.Prn232SE1819.Api.Infrastructure/Extensions/RepositoryExtensions.cs
+++ Com.FPTU.Prn232SE1819.Api.Infrastructure/Extensions/RepositoryExtensions.cs
@@ -25,4 +25,21 @@ public static class RepositoryExtensions
 
     public static Task<List<T>> ToListAsync<T>(this IRepository<T> repository, Expression<Func<T, bool>> predicate) where T : class
       => repository.Entities.Where(predicate).ToListAsync();
+
+    /*
+    3. Return 1 list entity T nao do ma co sap sep theo mot tieu chi nao do
+    */
+    public static IOrderedQueryable<T> OrderBy<T, Tkey>(this IRepository<T> repository, Expression<Func<T, Tkey>> keySelector) where T : class
+        => repository.Entities.OrderBy(keySelector);
+    /*
+     4. Return the first element
+     */
+    public static async Task<T> FirstOrDefaultAsync<T>(this IRepository<T> repository, Expression<Func<T, bool>> predicate) where T : class
+        => await repository.Entities.FirstOrDefaultAsync(predicate);
+    /*
+     5. Check xem co ton tai mot so entity thoa man mot tieu chi nao do hay khong
+     */
+
+    public static async Task<bool> AnySync<T>(this IRepository<T> repository, Expression<Func<T, bool>> predicate) where T : class
+        => await repository.Entities.AnyAsync(predicate);
 }
