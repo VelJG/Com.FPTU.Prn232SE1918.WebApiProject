56cd00ee6c2ed3a390bddcf842173488a939edc1
Bui Huu Dong <buihuudong19@gmail.com>
2026-06-22 12:49:24 +0700

    commit data caching

---
+++ Com.FPTU.Prn232SE1819.Api.Caching/DataCached.cs
@@ -0,0 +1,49 @@
+﻿namespace Com.FPTU.Prn232SE1819.Api.Caching;
+
+public class DataCached : IDataCached
+{
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Get<T>(string key, Func<T> acquire, int? cacheTime = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Get<T>(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object Get(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IList<string> GetKeys()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IList<T> GetValues<T>(string pattern)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsSet(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Set<T>(string key, T? value, int cacheTime)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Com.FPTU.Prn232SE1819.Api.Caching/IDataCached.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.FPTU.Prn232SE1819.Api.Caching;
+
+
+public interface IDataCached
+{
+    T Get<T>(string key, Func<T> acquire, int? cacheTime = null);
+    T Get<T>(string key);
+    object Get(string key);
+    IList<string> GetKeys();
+    IList<T> GetValues<T>(string pattern);
+
+    void Set<T>(string key, T? value, int cacheTime);
+    bool IsSet(string key);
+    
+    void Remove(string key);
+    void Clear();
+}
