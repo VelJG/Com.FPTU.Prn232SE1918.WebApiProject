68b4a658070eb3537a69ae14cbfc0300b2f0ca81
Bui Huu Dong <buihuudong19@gmail.com>
2026-06-22 14:00:01 +0700

    commit cache

--- Com.FPTU.Prn232SE1819.Api.Caching/DataCached.cs
+++ Com.FPTU.Prn232SE1819.Api.Caching/DataCached.cs
@@ -1,7 +1,31 @@
-﻿namespace Com.FPTU.Prn232SE1819.Api.Caching;
+﻿using Com.FPTU.Prn232SE1819.Api.Caching.common;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Primitives;
+using System.Collections.Concurrent;
+namespace Com.FPTU.Prn232SE1819.Api.Caching;
 
 public class DataCached : IDataCached
 {
+
+    private readonly IMemoryCache _memoryCache; // Tái sử dụng của microsoft
+
+    private static readonly ConcurrentDictionary<string, bool> _allKeys;
+    public CancellationTokenSource _cancellationTokenSource;
+
+    static DataCached()
+    {
+        _allKeys = new ConcurrentDictionary<string, bool>();
+    }
+    public DataCached(IMemoryCache memoryCache)
+    {
+        _memoryCache = memoryCache;
+        _cancellationTokenSource = new CancellationTokenSource();
+
+    }
+
+
+
+
     public void Clear()
     {
         throw new NotImplementedException();
@@ -9,17 +33,35 @@ public class DataCached : IDataCached
 
     public T Get<T>(string key, Func<T> acquire, int? cacheTime = null)
     {
-        throw new NotImplementedException();
+
+        //item already is in cached, so return it
+        if (_memoryCache.TryGetValue(key, out T? value))
+            return value!;
+
+        //or create it using passed function
+        var result = acquire();
+
+        //and set in cached (if cached time is defined)
+        if ((cacheTime ?? CachingCommonDefaults.CacheTime) > 0)
+        {
+            Set(key, result, cacheTime ?? CachingCommonDefaults.CacheTime);
+        }
+        return result;
     }
 
     public T Get<T>(string key)
     {
-        throw new NotImplementedException();
+        if (_memoryCache.TryGetValue(key, out T? value))
+            return value!;
+
+        return default(T)!; //from c# 9.0
     }
 
     public object Get(string key)
     {
-        throw new NotImplementedException();
+        if (_memoryCache.TryGetValue(key, out object? value))
+            return value!;
+        return null!;
     }
 
     public IList<string> GetKeys()
@@ -44,6 +86,26 @@ public class DataCached : IDataCached
 
     public void Set<T>(string key, T? value, int cacheTime)
     {
-        throw new NotImplementedException();
+        if (value != null)
+        {
+            _memoryCache.Set(_addKey(key), value,
+                _getMemoryCacheEntryOptions(TimeSpan.FromMinutes(cacheTime)));
+        }
+    }
+
+    private MemoryCacheEntryOptions _getMemoryCacheEntryOptions(TimeSpan cacheTime)
+    {
+        var options = new MemoryCacheEntryOptions()
+            .AddExpirationToken(new CancellationChangeToken(_cancellationTokenSource.Token))
+            .SetSize(0);
+
+        options.AbsoluteExpirationRelativeToNow = cacheTime;
+        return options;
+
+    }
+    private string _addKey(string key)
+    {
+        _allKeys.TryAdd(key, true);
+        return key;
     }
 }
---
+++ Com.FPTU.Prn232SE1819.Api.Caching/common/CachingCommonDefaults.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.FPTU.Prn232SE1819.Api.Caching.common;
+
+public static partial  class CachingCommonDefaults
+{
+    public static int CacheTime = 30;//15 minus
+
+    // fptu.ecommerce.{0}.id.{1} => fptu.ecommerce.product.id.1
+    public static string CacheKey => "fptu.ecommerce.{0}.id.{1}";
+    public static string AllCacheKey => "fptu.ecommerce.{0}.all";
+    public static string CacheKeyHeader => "fptu.ecommerce.{0}.id";
+
+    public static string CustomerByIdCacheKey => "fptu.ecommerce.customer.id.{0}";
+
+}
